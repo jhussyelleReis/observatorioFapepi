82b4a47b2fbb3c1af200651be6febb5e68bb78d1
jhussyellereis <jhussyelle.reis@gmail.com>
2018-02-26 09:49:55 -0300

    Adicionando novos modelos: Eventos, Publicacoes e Projetos.

--- app/admin.py
+++ app/admin.py
@@ -23,7 +23,28 @@ class ProgramaAdmin(admin.ModelAdmin):
     list_display = ('nome','descricao')
     search_fields = ['nome']
 
+class ProjetoAdmin(admin.ModelAdmin):
+    model = Projeto
+    list_display = ('titulo', 'pesquisador', 'vigencia')
+    list_filter = ['titulo', 'pesquisador', 'programa', 'vigencia']
+    search_fields = ['titulo']
+
+class PublicacaoAdmin(admin.ModelAdmin):
+    model = Publicacao
+    list_display = ('titulo', 'pesquisador', 'modalidade')
+    list_filter = ['titulo', 'pesquisador', 'modalidade']
+    search_fields = ['titulo']
+
+class EventoAdmin(admin.ModelAdmin):
+    model = Evento
+    list_display = ('titulo', 'pesquisador', 'pais', 'estado')
+    list_filter = ['titulo', 'pesquisador', 'estado']
+    search_fields = ['titulo']
+
 admin.site.register(Bolsa, BolsaAdmin)
 admin.site.register(Pesquisador, PesquisadorAdmin)
 admin.site.register(Programa, ProgramaAdmin)
 admin.site.register(Instituicao, InstituicaoAdmin)
+admin.site.register(Projeto, ProjetoAdmin)
+admin.site.register(Publicacao, PublicacaoAdmin)
+admin.site.register(Evento, EventoAdmin)
