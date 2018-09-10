305e87f80c454e0f40b16e2df41ed2bc5697be8f
jhussyellereis <jhussyelle.reis@gmail.com>
2018-09-10 08:59:23 -0300

    Removendo django material .

--- observatorioAdmin/settings.py
+++ observatorioAdmin/settings.py
@@ -31,9 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'material.theme.cyan',
-    'material',
-    'material.admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.humanize',
